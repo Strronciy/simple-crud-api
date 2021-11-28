f419ec69cc1b6991435f41792f99a37990b8d117
Strronciy <str0nciy@ya.ru>
2021-11-28 16:47:03 +0300

    init: add http server, webpack configs and scripts

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+// Create a local server to receive data from
+const server = http.createServer();
+
+// Listen to the request event
+server.on("request", (request, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+        JSON.stringify({
+            data: "Hello World!",
+        })
+    );
+});
+
+server.listen(PORT);
