b133d6d6bbdc0bdcef26e08ebc03fbffb21d1ba4
Strronciy <str0nciy@ya.ru>
2021-11-28 23:57:28 +0300

    fix:webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
+require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
     entry: "./src/index.js",
+    devServer: {
+        port: process.env.PORT,
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
